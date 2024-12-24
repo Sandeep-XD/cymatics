71554154f58ed77acdb002aa750735913e191799
Sandeep-XD <sanbofficial@gmail.com>
2024-12-24 18:29:44 +0530

    UI fixed

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -3,11 +3,11 @@ import Player from '../utilities/Player'
 const Song = () => {
   return (
     <>
-        <div className="preview__content__item md:max-w-[200px] sm:h-[50px] md:m-3">
-                <div className="player px-1 py-2 md:max-h-[112px] md:min-w-[220px]">
+        <div className="preview__content__item p-1 text-center">
+                <div className="player md:max-h-[20px] md:max-w-[170px]">
                     <Player/>
                 </div>
-                <div className="title">
+                <div className="title mt-5">
                     <h3 className='text-[#d9c180]'>All Star - 130 BPM G Min</h3>
                 </div>
             </div>
